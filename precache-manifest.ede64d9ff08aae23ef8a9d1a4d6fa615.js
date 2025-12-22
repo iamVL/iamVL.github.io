27d24dffca9197599c1ec3e07ea0cd524b2e8917
Vaishnavi Lokhande <slokhand@buffalo.edu>
2025-12-21 19:06:20 -0500

    Updates

---
+++ precache-manifest.ede64d9ff08aae23ef8a9d1a4d6fa615.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0434a99f09616fa00235124e41e0ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf6d2e47bf4f86de110b",
+    "url": "/static/css/main.289a9220.chunk.css"
+  },
+  {
+    "revision": "c1ccd3d8c642eca97bac",
+    "url": "/static/js/2.e955c7b1.chunk.js"
+  },
+  {
+    "revision": "0f22d104b7a9482e5430ec5fae55d31c",
+    "url": "/static/js/2.e955c7b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6d2e47bf4f86de110b",
+    "url": "/static/js/main.c5c58836.chunk.js"
+  },
+  {
+    "revision": "85b54b389594b3846b72",
+    "url": "/static/js/runtime-main.f91939a4.js"
+  },
+  {
+    "revision": "f901cb24d7b7f1fef9f5de190dafc1c5",
+    "url": "/static/media/Vaishnavi_Lokhande_FinalSOP.f901cb24.pdf"
+  },
+  {
+    "revision": "c70a388ba5b4c3aa9313ebd7454eff3e",
+    "url": "/static/media/Vaishnavi_Lokhande_PHD_CV.c70a388b.pdf"
+  },
+  {
+    "revision": "5c31f79da2bda21a452ca9cd96176da4",
+    "url": "/static/media/github-brands-solid.5c31f79d.svg"
+  },
+  {
+    "revision": "5781cdd40392025a9856d2d29f9341e0",
+    "url": "/static/media/linkedin-brands-solid.5781cdd4.svg"
+  },
+  {
+    "revision": "44eb4ede7aaad4022e655e9e680e381c",
+    "url": "/static/media/profile.44eb4ede.jpg"
+  },
+  {
+    "revision": "18ea591eb9304ab9d21f404783c88945",
+    "url": "/static/media/resume-solid.18ea591e.svg"
+  },
+  {
+    "revision": "20543490b86c45e6a055d7bfa782c0ca",
+    "url": "/static/media/statement-clipboard.20543490.svg"
+  }
+]);
